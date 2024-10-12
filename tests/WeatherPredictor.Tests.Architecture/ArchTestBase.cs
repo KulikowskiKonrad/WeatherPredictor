0616c84f821bab8a367bedfc39ee1bd5baf17088
Kulikowski Konrad <58163508+KulikowskiKonrad@users.noreply.github.com>
2024-10-12 17:20:16 +0200

    add architecture tests (#6)
    Co-authored-by: Kulikowski Konrad <kik@spyro-soft.com>

---
+++ tests/WeatherPredictor.Tests.Architecture/ArchTestBase.cs
@@ -0,0 +1,39 @@
+﻿using NetArchTest.Rules;
+using Xunit;
+
+namespace WeatherPredictor.Tests.Architecture;
+
+public abstract class ArchTestBase
+{
+    protected static void AssertAreImmutable(IEnumerable<Type> types)
+    {
+        IList<Type> failingTypes = new List<Type>();
+        foreach (var type in types)
+        {
+            if (type.GetFields().Any(x => !x.IsInitOnly) || type.GetProperties().Any(x => x.CanWrite))
+            {
+                failingTypes.Add(type);
+                break;
+            }
+        }
+
+        AssertFailingTypes(failingTypes);
+    }
+
+    protected static void AssertFailingTypes(IEnumerable<Type> types)
+    {
+        if (types == null)
+        {
+            Assert.Null(types);
+        }
+        else
+        {
+            Assert.Empty(types);
+        }
+    }
+
+    protected static void AssertArchTestResult(TestResult result)
+    {
+        AssertFailingTypes(result.FailingTypes);
+    }
+}
