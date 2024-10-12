ab6c73e781a90a8e665c75819a2643fa0d38ee51
Kulikowski Konrad <58163508+KulikowskiKonrad@users.noreply.github.com>
2024-10-12 17:21:56 +0200

    add database with repositories (#7)
    Co-authored-by: Kulikowski Konrad <kik@spyro-soft.com>

---
+++ src/WeatherPredictor.Infrastructure/DAL/WeatherPredictorDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeatherPredictor.Domain.Entities;
+
+namespace WeatherPredictor.Infrastructure.DAL;
+
+public class WeatherPredictorDbContext : DbContext
+{
+    public DbSet<Weather> Weathers { get; set; }
+
+    public WeatherPredictorDbContext(DbContextOptions<WeatherPredictorDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(WeatherPredictorInfrastructureAssembly.Assembly);
+    }
+}
